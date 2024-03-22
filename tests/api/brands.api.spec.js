dd3b5ab4fd28f03c78da3291dbb0c6b0266af2d3
staran <staran@griddynamics.com>
2024-03-22 21:22:23 +0200

    staran // add code samples

--- tests/api/brands.api.spec.js
+++ tests/api/brands.api.spec.js
@@ -2,7 +2,7 @@ import {expect, test} from "@playwright/test";
 import {config} from "../../config/config.js";
 
 
-test.skip('Get car brands', async()=>{
+test('TC-12 Get car brands', async()=>{
     const carBrandsResponse = await fetch(`${config.baseURL}api/cars/brands`)
     const body = await carBrandsResponse.json()
     expect(body).toEqual([])
